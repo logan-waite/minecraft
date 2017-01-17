9cf9e5f18ff2709fcbd9dd0d4eb51635af00f468
Logan waite <lwaite2010@gmail.com>
2017-01-17 16:37:29 -0700

    Update js to go to the right place

--- resources/javascript.js
+++ resources/javascript.js
@@ -2,7 +2,7 @@ $(function() {
     $('#ServerActions').submit(function(e) {
         e.preventDefault();
         var submit_value = $('#SubmitButton').val();
-        $.post("/minecraft/controllers/server_controller.php", {submit: submit_value}, function(result) {
+        $.post("/minecraft_server/minecraft/controllers/server_controller.php", {submit: submit_value}, function(result) {
             console.log(result);
             if (result == "success") {
                 window.location.reload();

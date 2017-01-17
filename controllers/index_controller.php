e827f8c0b445667859cfbf3ba5fb8f9bab76cb8d
Logan waite <lwaite2010@gmail.com>
2017-01-17 14:24:34 -0700

    Begin checking server if the minecraft process is running

--- controllers/index_controller.php
+++ controllers/index_controller.php
@@ -1,1 +1,13 @@
+<?php
 
+    $has_results = 0;
+    $running_processes = [];
+    exec("ps a | grep 'minecraft'", $running_processes, $has_results);
+
+    if($has_results) {
+        echo "<pre>";
+        var_dump($running_processes);
+        echo "</pre>";
+    }
+
+ ?>

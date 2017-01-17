ae590adc1a4f2b8fd35138025215b9ac8eac1276
Logan waite <lwaite2010@gmail.com>
2017-01-17 14:17:05 -0700

    Init

---
+++ views/index.php
@@ -0,0 +1,40 @@
+<?php require_once "../controllers/index_controller.php" ?>
+<html>
+    <head>
+        <!-- Latest compiled and minified Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Latest compiled and minified Jquery JavaScript -->
+        <script src="http://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+        <!-- Latest compiled and minified Bootstrap JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <!-- Fontawesome CDN -->
+        <script src="https://use.fontawesome.com/9fd33f348a.js"></script>
+
+        <!-- Local content -->
+        <link rel="stylesheet" href="../resources/stylesheet.css" type="text/css">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="page-header text-center">
+                <h1>Welcome to Logan's Minecraft Server</h1>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class='status-circle'>
+                                        <span>
+                                            Running
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>

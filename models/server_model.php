ba9d23299c821223024ebb88f3682b52d2e80057
Logan waite <lwaite2010@gmail.com>
2017-01-18 16:46:17 -0700

    More

--- models/server_model.php
+++ models/server_model.php
@@ -1,11 +1,15 @@
 <?php
 
 $script_path = "/var/www/html/minecraft_server/minecraft/resources/minecraft_server_script.sh";
+$shell_start = 'echo "import pty; pty.spawn(\'/bin/bash\')" > /tmp/asdf.py
+                python /tmp/asdf.py';
 
 function start_server() {
     global $script_path;
+    global $shell_start;
     $output = "";
     $failed = 1;
+    exec($shell_start);
     exec("sh {$script_path} start", $output, $failed);
 
     if ($failed) {
@@ -18,8 +22,10 @@ function start_server() {
 
 function stop_server() {
     global $script_path;
+    global $shell_start;
     $output = "";
     $failed = 1;
+
     exec("sh {$script_path} stop", $output, $failed);
 
     if ($failed) {

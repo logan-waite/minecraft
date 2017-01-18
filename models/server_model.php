a7035ec52663be1b7925d571b186d7373f73b7d5
Logan waite <lwaite2010@gmail.com>
2017-01-17 17:50:38 -0700

    Attempt to start minecraft server

--- models/server_model.php
+++ models/server_model.php
@@ -1,8 +1,11 @@
 <?php
+$server_start_script = "
+                        java -Xmx1024M -Xms1024M -jar /home/logan/minecraft/minecraft_server.jar nogui
+                        ";
 function start_server() {
     $output = "";
     $failed = 1;
-    exec("java -Xmx1024M -Xms1024M -jar /home/logan/minecraft/minecraft_server.jar nogui", $output, $failed);
+    exec($server_start_script, $output, $failed);
 
     if ($failed) {
         return "Unable to start server." + var_dump($output);
--- resources/javascript.js
+++ resources/javascript.js
@@ -2,6 +2,7 @@ $(function() {
     $('#ServerActions').submit(function(e) {
         e.preventDefault();
         var submit_value = $('#SubmitButton').val();
+        console.log('clicked!')
         $.post("/minecraft_server/minecraft/controllers/server_controller.php", {submit: submit_value}, function(result) {
             console.log(result);
             if (result == "success") {
